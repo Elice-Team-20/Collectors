6c2b8efe5c7b7d772af9f3c3759da1cf65d187b8
HyeRyeong <ryeong3725@gmail.com>
2022-05-24 17:20:56 +0900

    [feat] api 경로 수정

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -38,7 +38,7 @@ async function handleSubmit(e) {
   try {
     const data = { email, password };
 
-    const result = await Api.post('/api/login', data);
+    const result = await Api.post('/api/user/login', data);
     const token = result.token;
 
     // 로그인 성공, 토큰을 세션 스토리지에 저장
