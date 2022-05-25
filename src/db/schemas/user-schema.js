32f880446e97f0edbd4bb2a0ea7285ea5a553c42
[서울/2]김명진 <krocd923@gmail.com>
2022-05-25 18:23:08 +0900

    변수명 수정

--- src/db/schemas/user-schema.js
+++ src/db/schemas/user-schema.js
@@ -36,6 +36,10 @@ const UserSchema = new Schema(
       required: false,
       default: 'basic-user',
     },
+  orderInfo:{
+    type: Schema.Types.ObjectId,
+    ref: "orderinfoes",
+  },
   },
   {
     collection: 'users',
