5ed47e8e7b775dbf2c9b592a43202f3e6773b747
Kim Yeri <98sena@naver.com>
2022-05-26 03:05:15 +0900

    [feat] 상품 상세 페이지 #20

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -3,45 +3,63 @@
 // 코드 예시를 남겨 두었습니다.
 import { addCommas } from '/useful-functions.js';
 
-import { Nav } from '../components/Nav.js'; //네비게이션 컴포넌트
-import { Footer } from '../components/Footer.js'; //푸터 컴포넌트
+import { Nav } from '../../components/Nav.js'; //네비게이션 컴포넌트
+import { Footer } from '../../components/Footer.js'; //푸터 컴포넌트
 
-// GET / api/item/detail/?id= ...
+const nav = document.querySelector("nav")
+const footer = document.querySelector("footer")
 
-const ITEMDETAIL = document.querySelector('item-detail');
+const isLoggedIn = checkUser() // 로그인 상태 확인 
+addDefault(isLoggedIn); // navigation, footer 컴포넌트 넣기
 
+// url에서 id 값 추출해오기
+const ITEMDETAIL = document.querySelector('.item-detail');
 const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const id = urlParams.get('id'); // shortId?
-console.log(id);
-insertItemDetail(id);
+const id = new URLSearchParams(queryString).get('id');
+insertItemDetail(id);  // rendering
 
+// 로그인 상태 확인 함수
+function checkUser(){
+  return localStorage.getItem('token')? true : false
+}
+// navigation, footer Component 추가하는 역할
+function addDefault(isLoggedIn){
+  nav.innerHTML=Nav(isLoggedIn,"Home")
+  footer.innerHTML=Footer()
+}
+// 상세 페이지 렌더링 함수
 async function insertItemDetail(id) {
   const response = await fetch(`/api/item/${id}`);
   const details = await response.json();
 
-  const { itemName, category, manufacturingCompany, summary, mainExplanation, file, stocks, price, hashTag } = details;
-
+  const { itemName, category, manufacturingCompany, summary, mainExplanation, imgUrl, stocks, price, hashTag } = details;
+  console.log(imgUrl)
   ITEMDETAIL.insertAdjacentHTML(
     'beforeend',
     `
-  <div class="item">
-    <div>
-      <figure>
-        <img src="${file}" alt="item-image" />
-      </figure>
-    </div>
-    <div class="description">
-      <p>${manufacturingCompany}</p>
-      <div class="detail">
-        <h1>${itemName}</h1>
-        <p>${mainExplanation}</p>
+      <div class="item">
+        <div class="item-left">
+          <img src="${imgUrl}" alt="item-image" />
+        </div>
+        <div class="item-right">
+          <div class="item-description-box">
+            <div id="item-name">
+              ${itemName}
+            </div>
+            <div class="seperator"></div>
+            <div class="item-contents">
+              <div id="item-company">${manufacturingCompany}</div>
+              <div id="item-price">${addCommas(price)}</div>
+              <div id="item-explanation">
+              토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 토르의 망치는 무겁고 무겁고 쎄다. 
+              </div>
+            </div>
+          </div>
+          <div class="item-buttons-box">
+            <button id="addCartBtn">장바구니 추가하기</button>
+            <button id="buyNowBtn">바로 구매하기</button>
+          </div>
       </div>
-      <div class="price">
-        <h1>${addCommas(price)}</h1>
-      </div>
-    </div>
-  </div>
-  `
+    `
   );
 }
