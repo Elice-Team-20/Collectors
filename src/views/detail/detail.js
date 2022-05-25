2186df74ab0123bf4e213016ba98a8bac087d492
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-05-25 16:20:16 +0900

    [feat] 상품 목록 페이지 CSS 적용

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -1,12 +1,14 @@
 // 아래는 현재 home.html 페이지에서 쓰이는 코드는 아닙니다.
 // 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
 // 코드 예시를 남겨 두었습니다.
-import * as Api from '/api.js';
 import { addCommas } from '/useful-functions.js';
+
+import { Nav } from '../components/Nav.js'; //네비게이션 컴포넌트
+import { Footer } from '../components/Footer.js'; //푸터 컴포넌트
 
 // GET / api/item/detail/?id= ...
 
-const ITEMDETAIL = document.querySelector('detail-container');
+const ITEMDETAIL = document.querySelector('item-detail');
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -23,10 +25,10 @@ async function insertItemDetail(id) {
   ITEMDETAIL.insertAdjacentHTML(
     'beforeend',
     `
-  <div class="box product-item">
+  <div class="item">
     <div>
       <figure>
-        <img src="${file}" alt="clothes-image" />
+        <img src="${file}" alt="item-image" />
       </figure>
     </div>
     <div class="description">
--- src/views/list/list.js
+++ src/views/list/list.js
@@ -1,19 +1,31 @@
 // 아래는 현재 home.html 페이지에서 쓰이는 코드는 아닙니다.
 // 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
 // 코드 예시를 남겨 두었습니다.
-import * as Api from '/api.js';
 import { addCommas } from '/useful-functions.js';
 
-// const itemImage = document.querySelector('.item-img');
-// const manufacturer = document.querySelector('.manufacturer');
-// const Name = document.querySelector('.item-name');
-// const recommended = document.querySelector('.recommended');
-// const price = document.querySelector('.price');
-// const detail = document.querySelector('.detail');
+import { Nav } from '../components/Nav.js'; //네비게이션 컴포넌트
+import { Footer } from '../components/Footer.js'; //푸터 컴포넌트
 
 // GET / api/item/?id= ...
 
 const ITEMLIST = document.querySelector('.item-list');
+const nav = document.querySelector('nav');
+const footer = document.querySelector('footer');
+
+addDefault();
+addAllElements();
+addAllEvents();
+
+// Navigation Component 추가하는 역할
+function addDefault() {
+  nav.innerHTML = Nav(false, 'Register');
+  footer.innerHTML = Footer();
+}
+
+async function addAllElements() {}
+
+// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+function addAllEvents() {}
 
 insertItemElement();
 
@@ -22,32 +34,30 @@ async function insertItemElement() {
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   });
-  const item = await response.json();
-  console.log(item);
-
-  // {itemName, category, manufacturingCompany, summary, mainExplanation, file, stocks, price, hashTag }
-
-  const { itemName, category, manufacturingCompany, summary, mainExplanation, file, stocks, price, hashTag } = item;
-
-  ITEMLIST.insertAdjacentHTML(
-    'beforeend',
+  const items = await response.json();
+  console.log(items);
+  items.item.forEach(({ itemName, mainExplanation, imgUrl, price }) => {
+    console.log(imgUrl);
+    ITEMLIST.insertAdjacentHTML(
+      'beforeend',
+      `
+      <div class="item">
+        <div class="imgBox">
+          <figure>
+            <img id="itemImage" src="${imgUrl}" alt="item-image" />
+          </figure>
+        </div>
+        <div class="description">
+          <div class="detail">
+            <h1 id="itemName">${itemName}</h1>
+            <p id="mainExplanation">${mainExplanation}</p>
+          </div>
+          <div>
+            <h2 id="price">${addCommas(price)}원</h2>
+          </div>
+        </div>
+      </div>
     `
-    <div class="box product-item">
-      <div>
-        <figure>
-          <img id="productImage" src="${file}" alt="clothes-image" />
-        </figure>
-      </div>
-      <div class="description">
-        <div class="detail">
-          <h1 id="productTitle">${itemName}</h1>
-          <p id="productDescription">${mainExplanation}</p>
-        </div>
-        <div class="price">
-          <h1 id="productPrice">${addCommas(price)}</h1>
-        </div>
-      </div>
-    </div>
-  `
-  );
+    );
+  });
 }
