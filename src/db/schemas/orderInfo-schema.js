34fc598f363c4debac7f0ee105cd2f843332a19c
[서울/2]김명진 <krocd923@gmail.com>
2022-05-25 15:37:38 +0900

    orderinfo 스키마 작성

---
+++ src/db/schemas/orderInfo-schema.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from 'mongoose';
+
+const OrderInfo = new Schema({
+  shipAddress: {
+    type: String,
+    required: true,
+  },
+  totalCost: {
+    type: Number,
+     required: true
+    },
+  recipientName: {
+    type: String,
+     required: true}
+     ,
+  recipientPhone: {
+    type: Number,
+     required: true
+    },
+
+},
+{
+  collection: 'orderinfos',
+  timestamps :true,
+}
+);
+
+export default OrderInfo;
