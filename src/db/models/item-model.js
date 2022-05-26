baec1486396cf453cafee4d6d46a412686b48094
[SW2/서울/2반]김명진 <krocd923@gmil.com>
2022-05-26 14:22:12 +0900

    머지전 마지막 컨밴션 확인

--- src/db/models/item-model.js
+++ src/db/models/item-model.js
@@ -8,28 +8,29 @@ export class ItemModel {
 
     // 오브젝트 아이디 무조건 _ 하나여야 검증 가능
     //추후 async handler 추가 필요 - 일단 dev 에 넣기 위해 이렇게 처리
+
     try{
       const item = await Item.findOne({ _id: itemId}).exec();
-      if(item.id !== itemId){
-      console.log("에러 진입")
-      return new Error("잘못된 아이디입니다 아이디를 확인해주세요")
+      if(item.id !== itemId) {
+      console.log("에러 진입");
+      return new Error("잘못된 아이디입니다 아이디를 확인해주세요");
       }
     return item;
     }
     catch(er){
-      console.log("error 발생 개발자도구를 확인하세요")
-      return er
+      console.log("error 발생 개발자도구를 확인하세요");
+      return er;
     }
   }
 
-  async find(){
+  async find() {
     try{
       const item = await Item.find({}).exec();
       return item;
     }
-    catch(er){
-      console.log("error 발생 개발자도구를 확인하세요")
-      return er
+    catch(er) {
+      console.log("error 발생 개발자도구를 확인하세요");
+      return er;
     }
   }
 
@@ -39,7 +40,7 @@ export class ItemModel {
       return item;
     }
     catch(er){
-      return er
+      return er;
     }
   }
 
@@ -49,12 +50,12 @@ export class ItemModel {
       return item;
     }
     catch(er){
-      return er
+      return er;
     }
   }
 
-  async update(itemId, newInfo){
-    console.log(itemId, newInfo)
+  async update(itemId, newInfo) {
+    console.log(itemId, newInfo);
     const updatedItem = await Item.updateOne({id: itemId}, newInfo).exec();
     return updatedItem;
   }
