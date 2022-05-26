61941665ef8e1760a87ecf8607b12d3369d0a6cf
[SW2/서울/2반]김명진 <krocd923@gmil.com>
2022-05-26 14:14:40 +0900

    에러처리 코드 추가

--- src/db/models/item-model.js
+++ src/db/models/item-model.js
@@ -7,6 +7,7 @@ export class ItemModel {
   async findById(itemId) {
 
     // 오브젝트 아이디 무조건 _ 하나여야 검증 가능
+    //추후 async handler 추가 필요 - 일단 dev 에 넣기 위해 이렇게 처리
     try{
       const item = await Item.findOne({ _id: itemId}).exec();
       if(item.id !== itemId){
@@ -16,6 +17,7 @@ export class ItemModel {
     return item;
     }
     catch(er){
+      console.log("error 발생 개발자도구를 확인하세요")
       return er
     }
   }
@@ -26,18 +28,29 @@ export class ItemModel {
       return item;
     }
     catch(er){
-      return{er}
+      console.log("error 발생 개발자도구를 확인하세요")
+      return er
     }
   }
 
   async create(itemInfo) {
-    const item = await Item.create(itemInfo).exec();
-    return item;
+    try{
+      const item = await Item.create(itemInfo).exec();
+      return item;
+    }
+    catch(er){
+      return er
+    }
   }
 
   async delete(itemId) {
-    const item = await Item.findOneAndDelete({id: itemId}).exec();
-    return item;
+    try{
+      const item = await Item.findOneAndDelete({id: itemId}).exec();
+      return item;
+    }
+    catch(er){
+      return er
+    }
   }
 
   async update(itemId, newInfo){
@@ -45,7 +58,6 @@ export class ItemModel {
     const updatedItem = await Item.updateOne({id: itemId}, newInfo).exec();
     return updatedItem;
   }
-
 }
 
 const itemModel = new ItemModel();
