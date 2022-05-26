54f03be86fdd290eb61d6ca186bbb3a5b5da5f1e
[SW2/서울/2반]김명진 <krocd923@gmil.com>
2022-05-27 01:29:10 +0900

    라우터 선언

--- src/routers/orderInfo-router.js
+++ src/routers/orderInfo-router.js
@@ -3,8 +3,14 @@ import {orderInfoService} from './../services/index'
 const orderInfoRouter = Router();
 
 orderInfoRouter.post('/', (req, res) => {
-
-	res.send(orderInfoService.addOrderInfo())
+	const {shipAddress, totalCost, recipientName, recipientPhone} = req.body;
+	const inputOrderData = {
+		shipAddress: shipAddress,
+		totalCost: totalCost,
+		recipientName: recipientName,
+		recipientPhone: recipientPhone,
+	}
+	res.json(orderInfoService.addOrderInfo(inputOrderData))
 })
 
 export {orderInfoRouter}
