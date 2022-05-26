5886e9b027d36fb33783d95d8e1a5614bd83213b
Kim Yeri <98sena@naver.com>
2022-05-26 15:03:00 +0900

    [feat] logout, nav, footer 모듈화

--- src/views/list/list.js
+++ src/views/list/list.js
@@ -2,36 +2,27 @@
 // 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
 // 코드 예시를 남겨 두었습니다.
 import { addCommas } from '/useful-functions.js';
-import { checkUserStatus } from '../modules/user-status-functions.js'; // 로그인 상태 확인
 
-import { Nav } from '../components/Nav.js'; //네비게이션 컴포넌트
-import { Footer } from '../components/Footer.js'; //푸터 컴포넌트
+import { addNavEventListeners, addNavElements } from '../components/Nav/event.js';
+import { addFooterElements } from '../components/Footer/event.js';
 
 // GET / api/item/?id= ...
 
 const ITEMLIST = document.querySelector('.item-list');
-const nav = document.querySelector('nav');
-const footer = document.querySelector('footer');
-
-const isLoggedIn = checkUserStatus()
 
 addAllElements();
 addAllEvents();
 
-// Navigation Component 추가하는 역할
-function addDefault(isLoggedIn) {
-  nav.innerHTML = Nav(isLoggedIn, 'Register');
-  footer.innerHTML = Footer();
-}
-
 async function addAllElements() {
-  addDefault(isLoggedIn);
+  addNavElements();
+  addFooterElements();
+
   insertItemElement();
 }
 
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
-  
+  addNavEventListeners();
 }
 
 
