06dc9776f26ba12090e1cd654bad2a49c7b58c77
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-05-26 17:03:35 +0900

    [fix] 상품 목록 리스트 에러 수정 및 반응형 디자인 추가

--- src/views/list/list.js
+++ src/views/list/list.js
@@ -25,7 +25,6 @@ function addAllEvents() {
   addNavEventListeners();
 }
 
-
 async function insertItemElement() {
   const response = await fetch(`/api/item`, {
     method: 'GET',
@@ -33,7 +32,7 @@ async function insertItemElement() {
   });
   const items = await response.json();
   console.log(items);
-  items.item.forEach(({ _id, itemName, mainExplanation, imgUrl, price }) => {
+  items.item.forEach(({ _id, itemName, summary, imgUrl, price }) => {
     console.log(_id);
     ITEMLIST.insertAdjacentHTML(
       'beforeend',
@@ -48,7 +47,7 @@ async function insertItemElement() {
           <div class="description">
             <div class="detail">
               <h1 id="itemName">${itemName}</h1>
-              <p id="mainExplanation">${mainExplanation}</p>
+              <p id="summary">${summary}</p>
             </div>
             <div>
               <h2 id="price">${addCommas(price)}원</h2>
