b89b674f507d42d44adb90d2e0726394bda5860a
[SW2/서울/2반]김명진 <krocd923@gmil.com>
2022-05-26 15:36:24 +0900

    try module test

--- src/services/order-service.js
+++ src/services/order-service.js
@@ -1,15 +1,22 @@
-import { orderModel } from "../db";
+import { orderModel } from "../db/index";
 
 class OrderinfoService {
   constructor(orderModel){
-    this.orderModel = orderModel
+    this.orderModel = orderModel;
   }
 
-  async addOrderInfo(orderInfo){
-    const item = await this.orderModel.addOrderInfo(orderInfo)
-    return item
+  test(){
+    console.log(this.orderModel)
   }
+  // 피드백 받은 코드 반영
+  // async addOrderInfo(orderInfo){
+  //   return this.orderModel.crate(orderInfo)
+  // }
 
 
-  async getOrderInfo()
+
 }
+// 싱글톤
+const orderInfoService = new OrderinfoService(orderModel)
+orderInfoService.test()
+export {orderinfoService}
