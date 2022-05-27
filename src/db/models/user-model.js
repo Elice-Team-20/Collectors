c8434dc03e9848538b2e147188ed8fc47149f715
[SW2/서울/2반]김명진 <krocd923@gmil.com>
2022-05-27 17:43:26 +0900

    [refector]exec 추가 ,

--- src/db/models/user-model.js
+++ src/db/models/user-model.js
@@ -1,4 +1,3 @@
-import { path } from 'express/lib/application';
 import { model } from 'mongoose';
 import { UserSchema } from '../schemas/user-schema';
 
@@ -6,22 +5,22 @@ const User = model('users', UserSchema);
 
 export class UserModel {
   async findByEmail(email) {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).exec();
     return user;
   }
 
   async findById(userId) {
-    const user = await User.findOne({ _id: userId });
+    const user = await User.findOne({ _id: userId }).exec();
     return user;
   }
 
   async create(userInfo) {
-    const createdNewUser = await User.create(userInfo);
+    const createdNewUser = await User.create(userInfo).exec();
     return createdNewUser;
   }
 
   async findAll() {
-    const users = await User.find({});
+    const users = await User.find({}).exec();
     return users;
   }
 
@@ -29,12 +28,12 @@ export class UserModel {
     const filter = { _id: userId };
     const option = { returnOriginal: false };
 
-    const updatedUser = await User.findOneAndUpdate(filter, update, option);
+    const updatedUser = await User.findOneAndUpdate(filter, update, option).exec();
     return updatedUser;
   }
 
   async delete(userId) {
-    const removedUser = await User.findOneAndDelete({_id: userId});
+    const removedUser = await User.findOneAndDelete({_id: userId}).exec();
     return removedUser;
   }
 
@@ -42,7 +41,7 @@ export class UserModel {
     try{
       // findOneAndUpdate return the document _before_ `update` was applied
       //https://mongoosejs.com/docs/tutorials/findoneandupdate.html
-      await User.findOneAndUpdate({ email: userEmail }, {$push: {orderInfo: orderInfo}} )
+      await User.findOneAndUpdate({ email: userEmail }, {$push: {orderInfo: orderInfo}} ).exec()
       return await User.findOne({email: userEmail});
     }
     catch(er){
@@ -53,6 +52,7 @@ export class UserModel {
   async getUserAndPopulate(userId){
       try{
         const data = await User.findOne({_id: userId}).populate({path:'orderInfo'}).exec()
+
         return data;
       }
       catch(err){
