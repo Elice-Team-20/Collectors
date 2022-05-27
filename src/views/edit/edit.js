bf3fef29f60b09e893862b1709bfb5ed1a7e3308
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-05-27 15:00:11 +0900

    [feat] 회원 정보 수정 페이지 구현

---
+++ src/views/edit/edit.js
@@ -0,0 +1,81 @@
+import * as Api from '/api.js';
+import { validateEmail } from '/useful-functions.js';
+
+import { addNavEventListeners, addNavElements } from '../components/Nav/event.js';
+import { addFooterElements } from '../components/Footer/event.js';
+
+// 요소(element), input 혹은 상수
+const emailInput = document.querySelector('#emailInput');
+const passwordInput = document.querySelector('#passwordInput');
+const submitButton = document.querySelector('#submitButton');
+const addressButton = document.querySelector('#addressButton');
+const token = localStorage.getItem('token');
+
+addAllElements();
+addAllEvents();
+
+// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+async function addAllElements() {
+  addNavElements('Edit');
+  addFooterElements();
+
+  getUserDataToInput();
+}
+
+// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
+function addAllEvents() {
+  addNavEventListeners();
+  addressButton.addEventListener('click', handleAddress);
+}
+
+function handleAddress() {
+  new daum.Postcode({
+    oncomplete: function (data) {
+      let addr = ''; // 주소 변수
+      let extraAddr = ''; // 참고항목 변수
+
+      //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+      if (data.userSelectedType === 'R') {
+        // 사용자가 도로명 주소를 선택했을 경우
+        addr = data.roadAddress;
+      } else {
+        // 사용자가 지번 주소를 선택했을 경우(J)
+        addr = data.jibunAddress;
+      }
+
+      // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+      if (data.userSelectedType === 'R') {
+        // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+        // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+        if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+          extraAddr += data.bname;
+        }
+        // 건물명이 있고, 공동주택일 경우 추가한다.
+        if (data.buildingName !== '' && data.apartment === 'Y') {
+          extraAddr += extraAddr !== '' ? ', ' + data.buildingName : data.buildingName;
+        }
+        // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+        if (extraAddr !== '') {
+          extraAddr = ' (' + extraAddr + ')';
+        }
+        // 조합된 참고항목을 해당 필드에 넣는다.
+        document.getElementById('extraAddress').value = extraAddr;
+      } else {
+        document.getElementById('extraAddress').value = '';
+      }
+
+      // 우편번호와 주소 정보를 해당 필드에 넣는다.
+      document.getElementById('postcode').value = data.zonecode;
+      document.getElementById('address').value = addr;
+      // 커서를 상세주소 필드로 이동한다.
+      document.getElementById('detailAddress').focus();
+    },
+  }).open();
+}
+
+async function getUserDataToInput() {
+  const response = await Api.get('api/user/', token);
+  const userData = await response.json();
+
+  const { fullName, password, email, userAddress, userDetailAddress, userPostcode } = userData;
+}
