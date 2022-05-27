d6753c9e2ec324fc4dfeed1613bde55db8d38fce
HyeRyeong <ryeong3725@gmail.com>
2022-05-27 13:49:15 +0900

    [feat] 카테고리 수정, 삭제 기능 구현

--- src/routers/category-router.js
+++ src/routers/category-router.js
@@ -3,9 +3,14 @@ import { categoryService } from '../services';
 
 const categoryRouter = Router();
 
-categoryRouter.get('/', async (req, res) => {
-  const categories = await categoryService.getCategories();
-  res.json(categories);
+// 전체 카테고리들 리턴
+categoryRouter.get('/', async (req, res, next) => {
+  try{
+    const categories = await categoryService.getCategories();
+    res.json(categories);
+  }catch(error) {
+    next(error);
+  }
 })
 
 categoryRouter.post('/', async (req, res, next) => {
@@ -14,6 +19,27 @@ categoryRouter.post('/', async (req, res, next) => {
     const newCategory = await categoryService.addCategory(categoryName);
 
     res.json(newCategory);
+  }catch(error){
+    next(error);
+  }
+})
+
+categoryRouter.delete('/', async (req, res, next) => {
+  try{
+    const {categoryName} = req.body;
+    const deletedCategory = await categoryService.deleteCategory(categoryName);
+    res.json(deletedCategory);
+  }catch(error){
+    next(error);
+  }
+})
+
+categoryRouter.post('/:name', async (req, res, next) => {
+  try{
+    const newCategoryName = req.params.name;
+    const {categoryName} = req.body;
+    const updatedCategory = await categoryService.updateCategory(categoryName, newCategoryName);
+    res.json(updatedCategory);
   }catch(error){
     next(error);
   }
--- src/services/category-service.js
+++ src/services/category-service.js
@@ -6,15 +6,21 @@ class CategoryService {
     this.categoryModel = categoryModel;
   }
 
-  async addCategory(categoryName){
-    const findCategory = await this.categoryModel.findByName(categoryName);
+  //카테고리가 DB에 있는지 검사하는 함수.
+  async findCategory(name){
+    const findCategory = await this.categoryModel.findByName(name);
+    return findCategory ? true : false;
+  }
 
-    if(findCategory){
+  async addCategory(name){
+    const result = await this.findCategory(name);
+
+    if(!result){
+      const category = await categoryModel.create(name);
+      return category;
+    } else{
       throw new Error('카테고리가 이미 존재합니다. 다른 카테고리를 추가해주세요.');
     }
-
-    const category = await categoryModel.create(categoryName);
-    return category;
   }
 
   async getCategories(){
@@ -25,6 +31,29 @@ class CategoryService {
   async getCategoryByName(name) {
     const category = await categoryModel.findByName(name);
     return category;
+  }
+
+  async deleteCategory(name) {
+    const result = await this.findCategory(name);
+    
+    if (result) {// 카테고리가 존재할 경우
+      const deletedCategory = await categoryModel.delete(name);
+      return deletedCategory;
+    } else {
+      throw new Error('카테고리가 존재하지 않습니다.');
+    }
+  }
+
+  async updateCategory(oldname, newName){
+    const result = await this.findCategory(oldname);
+    console.log(result)
+
+    if (result) {
+      const updatedCategory = await categoryModel.update(oldname, newName);
+      return updatedCategory;
+    } else {
+      throw new Error('카테고리가 존재하지 않습니다.');
+    }
   }
 
 }
