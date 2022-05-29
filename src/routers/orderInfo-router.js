b45d4b09ac5c83ed35335577293a06e468dc30bc
[SW2/서울/2반]김명진 <krocd923@gmil.com>
2022-05-29 13:11:08 +0900

    [refector] 주석 추가

--- src/routers/orderInfo-router.js
+++ src/routers/orderInfo-router.js
@@ -29,23 +29,25 @@ orderInfoRouter.post('/', async(req, res) => {
   res.json(await orderInfoService.addOrderInfo(inputOrderData));
 })
 
+//db 에 등록된 주문 목록 전체를 조회하는 api
 orderInfoRouter.get('/', loginRequired, async(req, res) => {
   res.json(await orderInfoService.getOrderInfo());
 })
 
+// 주문id 로 주문을 조회하는 api
 orderInfoRouter.get('/:id', loginRequired, async(req, res) => {
   const {id} = req.params;
   res.json(await orderInfoService.getOrderInfoById(id));
 })
 
-// 주문 중복 해결해야함 => 프론트 단에서 하기로함
+// 결제를 누르면 주문 목록이 해당 유저에게 할당시키는 api
 orderInfoRouter.post('/makeOrder',loginRequired, async(req, res) => {
   const {email, orderId} = req.body;
   //res.json({email:email, orderid: orderId})
   res.json(await orderInfoService.connectOrderAndInfo(email, orderId));
 })
 
-//주문정보 업데이트 이거 body 에 담아야하나?
+//주문정보 업데이트
 orderInfoRouter.post('/update', loginRequired, async(req, res) => {
   const {orderId, updateInfo} = req.body;
   try{
@@ -56,6 +58,7 @@ orderInfoRouter.post('/update', loginRequired, async(req, res) => {
   }
 })
 
+// 주문 목록을 지우는 api
 orderInfoRouter.delete('/delete', loginRequired, async(req, res) => {
   const {orderId} = req.body;
   const deleteResult = await orderInfoService.deleteInfo(orderId);
