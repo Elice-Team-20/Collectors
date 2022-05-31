b61d062d4db7bd40aab1971adc2ba840cc2c3f04
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-06-01 02:11:09 +0900

    [fix] 회원 탈퇴 페이지 수정

--- src/views/resign/resign.js
+++ src/views/resign/resign.js
@@ -1,17 +1,20 @@
-import { addNavEventListeners, addNavElements, handleLogout } from '../components/Nav/event.js';
-import { addFooterElements } from '../components/Footer/event.js';
+import * as Api from '/api.js';
+import { selectElement } from '/useful-functions.js';
+
+import { addNavEventListeners, addNavElements, handleLogout } from '../../components/Nav/event.js';
+import { addFooterElements } from '../../components/Footer/event.js';
 
 // 요소(element), input 혹은 상수
-const submitButton = document.querySelector('#submitButton');
-const passwordInput = document.querySelector('#passwordInput');
 const token = localStorage.getItem('token');
+const submitButton = selectElement('#submitButton');
+const passwordInput = selectElement('#passwordInput');
 
 addAllElements();
 addAllEvents();
 
 // html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 async function addAllElements() {
-  addNavElements('Signout');
+  addNavElements('Resign');
   addFooterElements();
 }
 
@@ -28,14 +31,6 @@ async function handleSubmit(e) {
   // 유저 id 가져오기
   const userId = await findUserId();
   const password = passwordInput.value;
-
-  // 비밀번호 일치 여부 확인
-  // 변환된 비밀번호와 입력하는 비밀번호가 다른데 어떻게 일치 시키지..?
-  // 소셜 로그인의 경우 이 부분을 어떻게 구현해야 할지..
-  // if (passwordInput !== userPassword) {
-  //   alert('비밀번호가 일치하지 않습니다.');
-  //   return;
-  // }
 
   // 회원 탈퇴(삭제 요청)
   await fetch(`/api/user/delete/${userId}`, {
@@ -64,9 +59,7 @@ async function findUserId() {
       Authorization: `Bearer ${token}`,
     },
   });
-  console.log(response);
   const userId = await response.json();
-  console.log(userId);
 
   return userId;
 }
