92a969e2fc614e0147ec0ebb53c91a87daa7d7b0
Kim Yeri <98sena@naver.com>
2022-05-31 22:33:40 +0900

    [feat] 관리자 주문 내역 조회 페이지

--- src/views/adminOrderList/adminOrderList.js
+++ src/views/adminOrderList/adminOrderList.js
@@ -1,3 +1,5 @@
+import * as Api from '/api.js';
+
 import {
   addNavEventListeners,
   addNavElements,
@@ -7,9 +9,6 @@ import { addFooterElements } from '../../components/Footer/event.js';
 window.onload = () => {
   // admin인지 확인하기
 };
-const registerItemsBtn = document.querySelector('#registerItemsBtn');
-const manageItemsBtn = document.querySelector('#manageItemsBtn');
-const orderListBtn = document.querySelector('#orderListBtn');
 // 요소(element), input 혹은 상수
 addAllElements();
 addAllEvents();
@@ -23,4 +22,17 @@ async function addAllElements() {
 // 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
 function addAllEvents() {
   addNavEventListeners();
+}
+
+async function addOrderListElements() {
+  const orderList = await getOrderInfo();
+}
+
+async function getOrderInfo() {
+  try {
+    const orderList = await Api.get('/api/order');
+  } catch (err) {
+    console.error(err.stack);
+    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${err.message}`);
+  }
 }
