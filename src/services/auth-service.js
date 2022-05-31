4173b81aa17ae048e99d68ffbe977642933ff101
[SW2/서울/2반]김명진 <krocd923@gmil.com>
2022-05-31 15:36:53 +0900

    [refector] 함수 주석추가, 디버깅코드 제거

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -9,6 +9,8 @@ class KakaoOAuthService{
   async getUserByEmail(email){
     return await this.userService.getUserByEmail(email);
   }
+
+  // 카카오 토큰 요청 url 작성 함수
   makeUrlKakaoToken () {
     const REST_API_KEY = process.env.KAKAO_KEY
     const REDIRECT_URI = 'http://localhost:5000/api/auth/kakao/finish'
@@ -16,6 +18,7 @@ class KakaoOAuthService{
     return url
   }
 
+  // 카카오 에 토근 을 요청하고 사용자 정보 가져오는 함수
   async requestUser (config) {
       const baseUrl = "https://kauth.kakao.com/oauth/token";
       const params = new URLSearchParams(config).toString()
@@ -27,6 +30,7 @@ class KakaoOAuthService{
         },
       });
       const kakaoTokentRequestJson = await kakaoTokentRequest.json()
+
       // 토큰이 있다면 정보 달라고 요청
       if("access_token" in kakaoTokentRequestJson){
         const { access_token } = kakaoTokentRequestJson;
@@ -37,22 +41,20 @@ class KakaoOAuthService{
             }
           })
         const userReuestJson = await userReuest.json()
-
         return userReuestJson
       }
       else{
         throw new Error( "토큰이 없습니다." )
       }
   }
-// 회원인지 검사 개체지향에  책임을 분리원칙
-//..? 일단 분리 했는데 통합하는것이에 보기에 맞는거 같기도 하고
+  // 회원인지 검사 하는 함수
   async checkMember (userInfo) {
     const{email} = userInfo.kakao_account;
-    console.log(email)
     const getDBEmail = this.userService.getUserByEmail(email);
     return getDBEmail;
   }
 
+  //회원 우리db에 등록 하는 함수
   async signUp (userInfo){
     const { email, profile } = userInfo.kakao_account;
     const {nickname} = profile
@@ -64,13 +66,12 @@ class KakaoOAuthService{
     return await this.userService.addUser(userData);
   }
 
+  // 토큰을 만들어내느 함수
   async getToken(user){
     const secretKey = process.env.JWT_SECRET_KEY ||'secret-key'
-    console.log(user._id)
     const token = jwt.sign({ userId: user._id, role: user.role } , secretKey)
     return { token }
   }
-//
 }
 
 const kakaoOAuthService = new KakaoOAuthService(userService)
